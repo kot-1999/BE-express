e3cb2d2320ed7e066ff12bce4abe6de2a7601af9
alex <sashakashytskyy@gmail.com>
2024-12-23 10:20:29 +0000

    error middleware was created

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 
-import api from './api'
+import errorMiddleware from './middlewares/errorMiddleware'
+import authorizeRouters from './routes'
 
 const app = express()
 
 app.use(express.json())
-app.use(api())
+app.use(express.urlencoded({ extended: true }))
+// app.use('/api', api())
+app.use('/api', authorizeRouters())
+app.use(errorMiddleware)
 
 export default app
---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+
+import { IError } from '../types/error'
+
+export default function errorMiddleware(err: Error, req: Request, res: Response, _next: NewableFunction) {
+    // log an error
+    console.log('ERROR_MIDDLEWARE:', err.message)
+
+    let messages = []
+    let code = 500
+
+    if (err instanceof IError) {
+        code = err.statusCode
+
+        if (err.isJoi) {
+            messages = err.validationErrorItems
+        } else {
+            messages.push(err.message)
+        }
+    } else {
+        messages.push(err.message)
+    }
+
+    // return error response
+    return res.status(code).json({ messages })
+}
