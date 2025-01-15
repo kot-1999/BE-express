a8813284354294b1ac0abd9ee618f615112da042
alex <sashakashytskyy@gmail.com>
2025-01-15 20:47:38 +0000

    encryption functions were updated

--- src/services/Encryption.ts
+++ src/services/Encryption.ts
@@ -1,15 +1,20 @@
+import config from 'config'
 import crypto from 'crypto-js'
+
+import { IConfig } from '../types/config'
+
+const encryptionConfig = config.get<IConfig['encryption']>('encryption')
 
 export class EncryptionService {
     public static hashSHA256(value: string): string {
         return crypto.SHA256(value).toString()
     }
 
-    public static encryptAES(value: string, key: string): string {
-        return crypto.AES.encrypt(value, key).toString()
+    public static encryptAES(value: string): string {
+        return crypto.AES.encrypt(value, encryptionConfig.key).toString()
     }
 
-    public static decryptAES(value: string, key: string): string {
-        return crypto.AES.decrypt(value, key).toString(crypto.enc.Utf8)
+    public static decryptAES(value: string): string {
+        return crypto.AES.decrypt(value, encryptionConfig.key).toString(crypto.enc.Utf8)
     }
 }
