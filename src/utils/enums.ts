8bf0f97f98702b683a13f6b4a080d68e82f9f847
alex <sashakashytskyy@gmail.com>
2025-01-20 21:10:19 +0000

    EmailType enum was created

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -4,3 +4,8 @@ export enum PassportStrategy {
     jwtB2cForgotPassword = 'jwt-b2c-forgot_password',
     jwtB2b = 'jwt-b2b'
 }
+
+export enum EmailType {
+    forgotPassword = 'forgotPassword',
+    registered = 'registered'
+}
