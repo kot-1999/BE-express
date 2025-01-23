c3ece9817fde80352c3c4b02ae7a9f05506e16d5
alex <sashakashytskyy@gmail.com>
2025-01-23 16:36:02 +0000

    registration html email sending was implemented alongside with plain/text option

--- src/services/Email.ts
+++ src/services/Email.ts
@@ -1,5 +1,10 @@
+import * as path from 'node:path'
+
 import config from 'config'
+import ejs from 'ejs'
+import { compile, compiledFunction } from 'html-to-text'
 import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer'
 
 import { IConfig } from '../types/config'
 import { EmailDataType } from '../types/types'
@@ -9,6 +14,7 @@ import { IError } from '../utils/IError'
 class EmailService {
     private transporter: nodemailer.Transporter
     private config: IConfig['email']
+    private htmlToTextCompiler: compiledFunction
 
     private initTransporter(): void {
         this.transporter = nodemailer.createTransport({
@@ -28,28 +34,72 @@ class EmailService {
     constructor(config: IConfig['email']) {
         this.config = config
         this.initTransporter()
+        this.htmlToTextCompiler = compile()
+    }
+     
+    private async buildRegistered(data: EmailDataType<EmailType.registered>): Promise<Mail.Options & { html: string }> {
+        const templatePath = path.join(__dirname, 'emailTemplates', 'registration.ejs')
+        const templateData = {
+            firstName: data.firstName ?? 'dear customer',
+            lastName: data.lastName ?? '',
+            email: data.email
+        }
+
+        const htmlContent = await ejs.renderFile(templatePath, templateData)
+
+        const mailOptions = {
+            from: this.config.fromAddress,
+            to: data.email,
+            subject: 'Welcome to BE-proj!',
+            html: htmlContent
+        }
+
+        return mailOptions
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    private buildRegistered(data: EmailDataType<EmailType.registered>): void {}
+    private async buildForgotPassword(data: EmailDataType<EmailType.forgotPassword>)
+        : Promise<Mail.Options & { html: string }> {
+        const templatePath = path.join(__dirname, 'emailTemplates', 'registration.ejs')
+        const templateData = {
+            firstName: data.firstName ?? 'dear customer',
+            lastName: data.lastName ?? '',
+            email: data.email
+        }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    private buildForgotPassword(data: EmailDataType<EmailType.forgotPassword>): void {}
+        const htmlContent = await ejs.renderFile(templatePath, templateData)
+
+        const mailOptions = {
+            from: this.config.fromAddress,
+            to: data.email,
+            subject: 'Welcome to BE-proj!',
+            html: htmlContent
+        }
+
+        return mailOptions 
+    }
 
     public async sendEmail<T extends EmailType>(
         emailType: T,
         data: EmailDataType<T>
     ): Promise<void> {
+        let mailoptions
         switch (emailType) {
         case EmailType.registered:
-            this.buildRegistered(data as EmailDataType<EmailType.registered>)
+            mailoptions = await this.buildRegistered(data as EmailDataType<EmailType.registered>)
             break
         case EmailType.forgotPassword:
-            this.buildForgotPassword(data as EmailDataType<EmailType.forgotPassword>)
+            mailoptions = await this.buildForgotPassword(data as EmailDataType<EmailType.forgotPassword>)
             break
         default:
             throw new IError(500, `Unknown email type: ${emailType}`)
         }
+
+        await this.transporter.sendMail({
+            ...mailoptions,
+            text: this.htmlToTextCompiler(mailoptions.html) // text version of html
+        }).catch((err) => {
+            console.error('SENDING EMAIL ERROR', err)
+        })
     }
 }
 const emailConfig = config.get<IConfig['email']>('email')
